606e43cdff66920f184317120b713ab72930120e
Daniel-control <70458849+Daniel-control@users.noreply.github.com>
2021-06-29 16:09:39 +1000

    Rework
    yeah went through a rework

---
+++ q/src/Actors/Actor.gd
@@ -0,0 +1,10 @@
+extends KinematicBody2D
+class_name Actor
+
+var velocity: = Vector2.ZERO
+export var gravity: = 12500.0
+export var speed: = Vector2(300.0, 1000.0)
+
+func _physics_process(delta: float) -> void:
+	velocity.y = gravity * delta
+	velocity = move_and_slide(velocity)
