c50f30e5a51c0b031ad9ecbe06282beede51e001
Daniel-control <70458849+Daniel-control@users.noreply.github.com>
2021-06-30 14:39:31 +1000

    ye

--- q/src/Actors/Actor.gd
+++ q/src/Actors/Actor.gd
@@ -2,9 +2,8 @@ extends KinematicBody2D
 class_name Actor
 
 var velocity: = Vector2.ZERO
-export var gravity: = 12500.0
+export var gravity: = 100
 export var speed: = Vector2(300.0, 1000.0)
 
 func _physics_process(delta: float) -> void:
 	velocity.y = gravity * delta
-	velocity = move_and_slide(velocity)
